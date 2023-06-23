5255f637d59ddd79c396cd62037a101df5136b72
Cristian P <74473191+cpalomares01@users.noreply.github.com>
2023-06-23 16:30:42 -0700

    Update textBasedShop.cpp

--- textBasedShop.cpp
+++ textBasedShop.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 
+void pressAnyKeyToContinue()
+{
+    std::cout<<"Press any key to continue...\n> ";
+    
+    std::cin.get();
+}
+
 void introductionToTheGame()
 {
     //The lore behind the game.
     std::cout<<"Deep out in the woods rests a quiet little shop.\n"
     <<"It's known for selling three kinds of potions: Health Potions, Mana Potions, and Damage Potions.\n"
     <<"Your task is to go to the shop, and purchase potions. Good luck."<<std::endl;
-
-    system("pause");
+    
+    pressAnyKeyToContinue();
 }
 
 void shopChoices()
 {
     //Prints out the Shopkeeper's choices of potions.
     std::cout<<"Shopkeeper:\"Hello! What would you like to buy?\""<<std::endl;
-    std::cout<<"1.Health Potion\n2.Mana Potion\n3.Damage Potion";
+    std::cout<<"1.Health Potion\n2.Mana Potion\n3.Damage Potion\n";
 
-    system("pause");
+    pressAnyKeyToContinue();
 }
 
 int main()
 {
-    shopChoices();
+    introductionToTheGame();
 }
